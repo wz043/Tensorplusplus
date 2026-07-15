dfd063c7f2999b469f56e6b38542d1651a3fa73a
w臻 <61671162+wz043@users.noreply.github.com>
2026-07-16 00:13:38 +0800

    Add files via upload

---
+++ Tensorplusplus/Tensorplusplus.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Tensor/Tensor.h"
+
+#include "bs/TPP_bs.h"
+
+#include "Function/TPP_function.h"
+
+#include "Loss/TPP_loss.h"
+
+#include "Optimizer/TPP_optimizer.h"
+
+#include "Resource/Resource.h"
